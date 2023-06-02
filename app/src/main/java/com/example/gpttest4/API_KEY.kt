3b3f3f02686b490503d2292f1cd60803d6f798be
gurumedev <gurumdevv@gmail.com>
2023-06-02 15:04:53 +0900

    ui 작성 및 최적화

---
+++ app/src/main/java/com/example/gpttest4/API_KEY.kt
@@ -0,0 +1,5 @@
+package com.example.gpttest4
+
+object API_KEY {
+    const val MY_API_KEY = "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"
+}
--- app/src/main/java/com/example/gpttest4/MainActivity.kt
+++ app/src/main/java/com/example/gpttest4/MainActivity.kt
@@ -3,31 +3,35 @@ package com.example.gpttest4
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.KeyEvent
 import android.view.inputmethod.EditorInfo
 import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.android.volley.RequestQueue
-import com.android.volley.Response
-import com.android.volley.RetryPolicy
-import com.android.volley.VolleyError
-import com.android.volley.toolbox.JsonObjectRequest
-import com.android.volley.toolbox.Volley
+import com.example.gpttest4.API_KEY.MY_API_KEY
 import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody.Companion.toRequestBody
+import org.json.JSONArray
 import org.json.JSONObject
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var textInputLayout: TextInputLayout
     lateinit var queryEdt: TextInputEditText
     lateinit var messageRV: RecyclerView
     lateinit var messageRVAdapter: MessageRVAdapter
     lateinit var messageList: ArrayList<MessageRVModal>
-    val url = "https://api.openai.com/v1/completions"
-
-    val OPENAI_API_KEY = "..."
-
+    lateinit var question: String
+    private val client = OkHttpClient()
 
     @SuppressLint("NotifyDataSetChanged")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -41,64 +45,84 @@ class MainActivity : AppCompatActivity() {
         val layoutManager = LinearLayoutManager(applicationContext)
         messageRV.layoutManager = layoutManager
         messageRV.adapter = messageRVAdapter
+        textInputLayout = findViewById(R.id.idTILQuery)
 
-        queryEdt.setOnEditorActionListener(TextView.OnEditorActionListener{ textView: TextView, i, keyEvent: KeyEvent? ->
+        queryEdt.setOnEditorActionListener(TextView.OnEditorActionListener{ textView: TextView, i: Int, keyEvent: KeyEvent? ->
             if(i == EditorInfo.IME_ACTION_SEND){
-                if(queryEdt.text.toString().isNotEmpty()) {
-                    messageList.add(MessageRVModal(queryEdt.text.toString(), "user"))
-                    messageRVAdapter.notifyDataSetChanged()
-                    getResponse(queryEdt.text.toString())
-                } else {
-                    Toast.makeText(this, "Please enter your query", Toast.LENGTH_SHORT).show()
-                }
+                processData()
                 return@OnEditorActionListener true
             }
             false
-
         })
+
+        textInputLayout.setEndIconOnClickListener {
+            processData()
+        }
     }
 
-    private fun getResponse(query: String) {
-        queryEdt.setText("")
-        val queue: RequestQueue = Volley.newRequestQueue(applicationContext)
-        val jsonObject = JSONObject()
-        jsonObject.put("model", "text-davinci-003")
-        jsonObject.put("prompt", query)
-        jsonObject.put("temperature", 0)
-        jsonObject.put("max_tokens", 100)
+    private fun processData() {
+        if(queryEdt.text.toString().isNotEmpty()) {
+            question = queryEdt.text.toString()
+            messageList.add(MessageRVModal(question, "user"))
+            messageRVAdapter.notifyDataSetChanged()
 
-        val postRequest: JsonObjectRequest =
-            @SuppressLint("NotifyDataSetChanged")
-            object: JsonObjectRequest(Method.POST, url, jsonObject, Response.Listener { response ->
-                val responseMsg: String = response.getJSONArray("choices").getJSONObject(0).getString("text")
-                messageList.add(MessageRVModal(responseMsg, "bot"))
-                println(responseMsg)
-                messageRVAdapter.notifyDataSetChanged()
-
-            }, Response.ErrorListener {
-                Toast.makeText(applicationContext, "Fail to get response..", Toast.LENGTH_SHORT).show()
-            }) {
-                override fun getHeaders(): MutableMap<String, String> {
-                    val params: MutableMap<String, String> = HashMap()
-                    params["Content-Type"] = "application/json"
-                    params["Authorization"] = "Bearer $OPENAI_API_KEY"
-                    return params
+            getResponse(queryEdt.text.toString()) { response ->
+                runOnUiThread {
+                    messageList.add(MessageRVModal(response, "bot"))
+                    Log.v("data", response)
+                    messageRVAdapter.notifyDataSetChanged()
                 }
             }
 
-        postRequest.retryPolicy = object: RetryPolicy {
-            override fun getCurrentTimeout(): Int {
-                return 50000
-            }
-
-            override fun getCurrentRetryCount(): Int {
-                return 50000
-            }
-
-            override fun retry(error: VolleyError?) {
-
-            }
+        } else {
+            Toast.makeText(this, "문장을 입력하세요", Toast.LENGTH_SHORT).show()
         }
-        queue.add(postRequest)
+    }
+
+    private fun getResponse(qustion: String, callback: (String) -> Unit) {
+        queryEdt.setText("")
+
+        val url = "https://api.openai.com//v1/chat/completions"
+
+        val requestBody = """
+            {
+            "model": "gpt-3.5-turbo",
+            "messages": [{"role": "user", "content": "${qustion} 반말로 대답해줘"}]
+            }
+        """.trimIndent()
+
+        val request = Request.Builder()
+            .url(url)
+            .header( "Content-Type", "application/json")
+            .addHeader("Authorization", "Bearer $MY_API_KEY")
+            .post(requestBody.toRequestBody("application/json".toMediaTypeOrNull()))
+            .build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                Log.e("error", "API FALILED", e)
+                Toast.makeText(this@MainActivity, "현재 네트워크가 불안정합니다. 잠시 후 시도해주세요.",
+                    Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onResponse(call: Call, response: okhttp3.Response) {
+                val body = response.body?.string()
+
+                if (body != null) {
+                    Log.v("data", body)
+                } else {
+                    Log.v("data", "empty")
+                    Toast.makeText(this@MainActivity, "현재 네트워크가 불안정합니다. 잠시 후 시도해주세요.",
+                        Toast.LENGTH_SHORT).show()
+                }
+
+
+                val jsonObject = JSONObject(body)
+                val jsonArray: JSONArray = jsonObject.getJSONArray("choices")
+                val textResult = jsonArray.getJSONObject(0).getJSONObject("message").getString("content")
+                callback(textResult)
+
+            }
+        })
     }
 }
